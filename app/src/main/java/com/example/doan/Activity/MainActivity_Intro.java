167c0d12f4f71ce031e04ca1b3faaa73fa38edce
Khanh3012002 <tungvipduong@gmail.com>
2024-03-04 18:09:33 +0700

    day 4/3/2024

--- app/src/main/java/com/example/doan/Activity/MainActivity_Intro.java
+++ app/src/main/java/com/example/doan/Activity/MainActivity_Intro.java
@@ -1,14 +1,16 @@
-package com.example.doan;
+package com.example.doan.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.doan.R;
+
+public class MainActivity_Intro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
     }
 }
